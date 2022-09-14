46cd620eb84c12eaffebb76feed8213bebe13133
mr-riz <rahulsharmaofficial356@gmail.com>
2022-09-14 18:24:06 +0530

    UserLoginFile

---
+++ src/main/java/com/example/demo/UserRegistrationFormApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegistrationFormApplication.class, args);
+		
+		
+		
+	}
+
+}
+ 
---
+++ src/test/java/com/example/demo/UserRepositoryTests.java
@@ -0,0 +1,56 @@
+package com.example.demo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+
+public class UserRepositoryTests {
+	 @Autowired
+	 private UserRepository repo;
+	 
+	 @Autowired
+	 
+	 private TestEntityManager entityManager;
+	 
+	 @Test
+	  public void testCreateUser()
+	  {
+		 User user=new User();
+		 user.setEmail("papu@gmail.com");
+		 user.setPassword("hoho8989");
+		 
+		 user.setFirstName("Papu");
+		 user.setLastName("Alexeder");
+		 
+		User savedUser= repo.save(user);
+		
+		User exitUser=entityManager.find(User.class, savedUser.getId());
+		
+		
+	assertThat(exitUser.getEmail()).isEqualTo(user.getEmail());
+	  }
+	
+	@Test
+	public void testFindUserByEmail()
+	{
+		String email="name@milkbasket.com";
+		
+		User user=repo.findByEmail(email);
+		
+		assertThat(user).isNotNull();
+	
+	
+	  }
+	  
+
+}
